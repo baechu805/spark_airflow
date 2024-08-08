038f6458441a3cccb58d90ad4e0eb305814dad0b
baechu805 <bjoo606@naver.com>
2024-08-08 17:44:12 +0900

    a

--- pyspark/simple.py
+++ pyspark/simple.py
@@ -16,6 +16,7 @@ logFile = "/home/joo/app/spark-3.5.1-bin-hadoop3/README.md"  # Should be some fi
 df1 = spark.read.parquet(f"/home/joo/data/movie/repartition/load_dt={LOAD_DT}")
 df1.createOrReplaceTempView("df1")
 
+result_df = spark.sql("""
 SELECT 
     movieCd, -- 영화의 대표코드
     movieNm,
@@ -26,5 +27,8 @@ SELECT
     repNationCd, -- 한국/외국 영화별로 조회할 수 있습니다. “K: : 한국영화 “F” : 외국영화
     load_dt
 FROM df1
+""")
+
+result_df.show()
 
 spark.stop()
