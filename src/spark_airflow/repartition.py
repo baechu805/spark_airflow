6800ea016e70e3d6b737468c0facfd3895da9701
baechu805 <bjoo606@naver.com>
2024-08-08 22:47:49 +0900

    a

--- src/spark_airflow/repartition.py
+++ src/spark_airflow/repartition.py
@@ -4,8 +4,8 @@ import pandas as pd
 def re_partition(load_dt):
     home_dir = os.path.expanduser("~")
     read_path = f'{home_dir}/data/movie/movie_data/data/extract/load_dt={load_dt}'
-    write_base = f'{home_dir}/data/movie/repartition'
-    write_path = f'{write_base}/load_dt={load_dt}'
+    write_base = f'{home_dir}/data/movie/repartition'  # 상위 디렉토리 - 데이터 저장의 기본 위치
+    write_path = f'{write_base}/load_dt={load_dt}'  # 하위 디렉토리 - 날짜별,측정 기준으로 파티션된 최종적으로 저장할 구체적인 디렉토리
 
     df = pd.read_parquet(read_path)
     df['load_dt']=load_dt
